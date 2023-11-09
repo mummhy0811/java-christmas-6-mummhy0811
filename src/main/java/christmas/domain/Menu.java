ec8cf0b0ed98a3b33ec517edd6ff5091d7e14c5e
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-10 01:47:29 +0900

    Feat: 메뉴판 메뉴 enum에 저장

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,39 @@
+package christmas.domain;
+
+import christmas.constant.ErrorConstant;
+
+
+public enum Menu {
+    MUSHROOM_SOUP("애피타이저", "양송이스프", 6000),
+    TAPAS("애피타이저", "타파스", 5500),
+    CAESAR_SALAD("애피타이저", "시저샐러드", 8000),
+
+    T_BONE_STEAK("메인","티본스테이크", 5500),
+    BARBECUERIBS("메인","바비큐립", 5400),
+    SEAFOOD_PASTA("메인","해산물파스타", 3500),
+    CHRISTMAS_PASTA("메인","크리스마스파스타", 2500),
+
+    CHOCO_CATEK("디저트", "초코케이크", 15000),
+    ICECREAM("디저트", "아이스크림", 5000),
+
+    ZERO_COKE("음료", "제로콜라", 3000),
+    RED_WINE("음료", "레드와인", 60000),
+    CHAMPAGNE("음료", "샴페인", 2500);
+
+    private String type;
+    private String name;
+    private Integer price;
+
+    Menu(String type, String name,Integer price){
+        this.type=type;
+        this.name=name;
+        this.price=price;
+    }
+    public static Menu find(String input) {
+        try{
+            return Menu.valueOf(input);
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(ErrorConstant.INVALIDATE_ORDER);
+        }
+    }
+}
