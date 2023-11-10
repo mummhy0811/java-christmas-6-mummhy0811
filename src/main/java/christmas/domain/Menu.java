196439d967a0ce6e152c2214093cc2f9548f8126
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-10 20:29:26 +0900

    Fix: 메뉴 가격 오타 수정

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -10,17 +10,17 @@ public enum Menu {
     TAPAS("애피타이저", "타파스", 5500),
     CAESAR_SALAD("애피타이저", "시저샐러드", 8000),
 
-    T_BONE_STEAK("메인","티본스테이크", 5500),
-    BARBECUERIBS("메인","바비큐립", 5400),
-    SEAFOOD_PASTA("메인","해산물파스타", 3500),
-    CHRISTMAS_PASTA("메인","크리스마스파스타", 2500),
+    T_BONE_STEAK("메인","티본스테이크", 55000),
+    BARBECUERIBS("메인","바비큐립", 54000),
+    SEAFOOD_PASTA("메인","해산물파스타", 35000),
+    CHRISTMAS_PASTA("메인","크리스마스파스타", 25000),
 
     CHOCO_CATEK("디저트", "초코케이크", 15000),
     ICECREAM("디저트", "아이스크림", 5000),
 
     ZERO_COKE("음료", "제로콜라", 3000),
     RED_WINE("음료", "레드와인", 60000),
-    CHAMPAGNE("음료", "샴페인", 2500);
+    CHAMPAGNE("음료", "샴페인", 25000);
 
     private String type;
     private String name;
