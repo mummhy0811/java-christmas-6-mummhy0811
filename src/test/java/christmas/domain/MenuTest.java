4b0a007b763307fb6c94d10a558e7879486a9a3e
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-15 00:02:31 +0900

    Test: 메뉴 테스트코드 작성

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -1,4 +1,24 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MenuTest {
-  
+    @Test
+    public void testFindValidMenu() {
+        Menu foundMenu = Menu.find("아이스크림");
+
+        assertEquals("아이스크림", foundMenu.getName());
+        assertEquals(Integer.valueOf(5000), foundMenu.getPrice());
+        assertEquals("디저트", foundMenu.getType());
+    }
+
+    @Test()
+    public void testFindInvalidMenu() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Menu.find("없는메뉴");
+        });
+    }
+
 }
